12aed509251608d9f2f4fdcff3c471ad810e5c95
Thelonious <thelonious06@gmail.com>
2024-02-05 09:26:19 -0800

    began project

---
+++ js/app.js
@@ -0,0 +1,29 @@
+/*----- constants -----*/
+const pet = {
+    hunger: 100,
+    sleep: 100,
+    activity: 100
+}
+
+
+
+/*----- app's state (variables) -----*/
+
+
+
+
+
+/*----- cached element references -----*/
+
+
+
+
+
+/*----- event listeners -----*/
+
+
+
+
+
+
+/*----- functions -----*/
