86d0832a405e7ab566b54d770ba12a3e8155cf90
Thelonious <thelonious06@gmail.com>
2024-02-08 12:50:10 -0800

    added changing sprite

--- js/app.js
+++ js/app.js
@@ -10,7 +10,10 @@ let hungerDec;
 let sleepDec;
 let activityDec;
 let dayCounter;
+let imageChanger;
 let dayNum = 1;
+let actionPoints = 10;
+let statsAverage;
 
 /*----- cached element references -----*/
 let hungerStat = document.querySelector('#hunger');
@@ -27,6 +30,7 @@ let nameTag = document.querySelector('#name');
 let inputBox = document.querySelector('#input');
 const sprite = document.querySelector('#sprite');
 const dropdownMenu = document.querySelector('#dropdown-menu')
+const currentPoints = document.querySelector('#action-points')
 /*----- event listeners -----*/
 feedBtn.addEventListener('click', hungerInc);
 sleepBtn.addEventListener('click', sleepInc);
@@ -39,67 +43,107 @@ dropdownMenu.addEventListener('change', dropdownSelection)
 //Start game function
 
 function initialize() {
-     hungerDec = setInterval(function () {
+    hungerDec = setInterval(function () {
         pet.hunger -= 1
         if (pet.hunger === 0) {
             stopTimers()
             youLose()
         }
         renderStats()
-    }, 1000)
-    
-     sleepDec = setInterval(function () {
+        updateAverage()
+    }, 1500)
+
+    sleepDec = setInterval(function () {
         pet.sleep -= 1
         if (pet.sleep === 0) {
             stopTimers()
             youLose()
         }
         renderStats()
-    }, 5000)
-    
-     activityDec = setInterval(function () {
+        updateAverage()
+    }, 2000)
+
+    activityDec = setInterval(function () {
         pet.activity -= 1
         if (pet.activity === 0) {
             stopTimers()
             youLose()
         }
         renderStats()
-    }, 3000)
+        updateAverage()
+    }, 1000)
 
-     dayCounter = setInterval(function () {
+    dayCounter = setInterval(function () {
         dayNum += 1
+        actionPoints += 3
         renderDay()
+        renderAP()
         if (dayNum === 15) {
             stopTimers()
             youWin()
         }
     }, 30000)
 
+    if (dropdownMenu.value === 'https://i.imgur.com/lwusw0B.png') {
+        imageChanger = setInterval(function() {
+            if (statsAverage >= 60) {
+                sprite.src = 'https://i.imgur.com/lwusw0B.png'
+            } else if (statsAverage < 60 && statsAverage >= 30) {
+                sprite.src = 'https://i.imgur.com/4aRsznK.png'
+            } else if (statsAverage < 30) {
+                sprite.src = 'https://i.imgur.com/sHQI6w7.png'
+            }
+            console.log(statsAverage)
+        }, 500)
+    }
+
     renderStats()
     renderDay()
+    renderAP()
     dropdownSelection()
-    
+
     nameTag.innerHTML = document.querySelector('input').value
     document.querySelector('input').value = ''
 
     toggleDisplay([startBtn, inputBox, dropdownMenu], 'none')
-    toggleDisplay([sprite, feedBtn, sleepBtn, playBtn, currentDay], '')
+    toggleDisplay([sprite, feedBtn, sleepBtn, playBtn, currentDay, currentPoints], '')
 }
 
 
 //Increase stats
 
 function hungerInc() {
-    pet.hunger <= 97 ? pet.hunger += 3 : pet.hunger = 100;
-    renderStats()
+    if (actionPoints === 0) {
+        return
+    } else {
+        pet.hunger <= 97 ? pet.hunger += 3 : pet.hunger = 100;
+        renderStats()
+        actionPoints -= 1
+        renderAP()
+    }
+
 }
 function sleepInc() {
-    pet.sleep <= 92 ? pet.sleep += 8 : pet.sleep = 100;
-    renderStats()
+    if (actionPoints === 0) {
+        return
+    } else {
+        pet.sleep <= 92 ? pet.sleep += 8 : pet.sleep = 100;
+        renderStats()
+        actionPoints -= 1
+        renderAP()
+    }
+
 }
 function activityInc() {
-    pet.activity <= 95 ? pet.activity += 5 : pet.activity = 100;
-    renderStats()
+    if (actionPoints === 0) {
+        return
+    } else {
+        pet.activity <= 95 ? pet.activity += 5 : pet.activity = 100;
+        renderStats()
+        actionPoints -= 1
+        renderAP()
+    }
+
 }
 //Render functions
 
@@ -114,8 +158,17 @@ function renderDay() {
     currentDay.innerHTML = `Day ${dayNum}`
 }
 
+function renderAP() {
+    currentPoints.innerHTML = `Action points: ${actionPoints}`
+}
+
+function updateAverage() {
+    statsAverage = (pet.hunger + pet.sleep + pet.activity) / 3
+}
+
 function resetGame() {
     stopTimers()
+    actionPoints = 10
     dayNum = 1
     pet.activity = 100
     pet.sleep = 100
@@ -125,7 +178,7 @@ function resetGame() {
     resultMsg.innerHTML = ''
     nameTag.innerHTML = ''
     toggleDisplay([startBtn, inputBox, dropdownMenu], '')
-    toggleDisplay([feedBtn, sleepBtn, playBtn, currentDay, sprite, hungerStat, sleepStat, activityStat], 'none')
+    toggleDisplay([feedBtn, sleepBtn, playBtn, currentDay, sprite, hungerStat, sleepStat, activityStat, currentPoints], 'none')
 }
 
 
@@ -135,15 +188,16 @@ stopTimers = function () {
     clearInterval(activityDec)
     clearInterval(sleepDec)
     clearInterval(dayCounter)
+    clearInterval(imageChanger)
 }
 
 //loss message
-youLose = function() {
+youLose = function () {
     resultMsg.innerHTML = "You  Lose!"
 }
 
 //win message
-youWin = function() {
+youWin = function () {
     resultMsg.innerHTML = "You  Win!"
 }
 
@@ -154,10 +208,10 @@ function dropdownSelection() {
 
 //change display value of cached elements passed through as array
 function toggleDisplay(n, displayValue) {
-    n.forEach(function(n) {
+    n.forEach(function (n) {
         n.style.display = displayValue
     })
-    
+
 }
 //initial display
 toggleDisplay([feedBtn, sleepBtn, playBtn, sprite], 'none')
